2d44cd5011f7e99d318fab330693c49fd10ba400
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-03-26 17:29:58 +0600

    basic project done

--- js/api.js
+++ js/api.js
@@ -11,7 +11,7 @@ const loadSingleData = async (id) => {
     const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    displaySingleData(data);
+    displaySingleData(data.data);
 }
 
 const processWithData = (data, limit) => {
@@ -60,9 +60,96 @@ cardContainer.appendChild(div);
 
 }
 
+// modal card dynamic data
 
 const displaySingleData = data =>{
+const modalContainer = document.getElementById('modal-card-container');
+console.log(modalContainer);
+modalContainer.textContent = "";
 
+const divLeft = document.createElement('div');
+divLeft.classList.add('card');
+divLeft.classList.add('card-bordered');
+
+const divRight = document.createElement('div');
+divRight.classList.add('card');
+divRight.classList.add('card-bordered');
+
+console.log(data);
+
+divLeft.innerHTML = `
+
+<div class="card-body">
+
+<h2 class="card-title text-2xl font-bold">
+  ${data.description}
+</h2>
+
+<div class="flex gap-6">
+
+<div class="small-vanilla-card rounded-lg mt-8">
+<p class="pl-6 pt-5"><span class=" text-lg text-green-600 font-bold">${data.pricing[0].price}</span>
+<span class="py-3 text-lg text-green-600 font-bold">${data.pricing[0].plan}</span> 
+</p>
+</div>
+
+<div class="small-vanilla-card rounded-lg mt-8">
+<p class="pl-6 pt-5"><span class=" text-lg text-orange-500 font-bold">${data.pricing[1].price}</span>
+<span class="py-3 text-lg text-orange-500 font-bold">${data.pricing[1].plan}</span> 
+</p>
+</div>
+
+<div class="small-vanilla-card rounded-lg mt-8">
+<p class="pl-6 pt-4"><span class=" text-lg text-red-700 font-bold">${data.pricing[2].price}</span>
+<span class="py-3 text-lg text-red-700 font-bold">${data.pricing[2].plan}</span> 
+</p>
+</div>
+</div>
+
+<div class="mt-5 flex justify-between">
+
+<div>
+<h2 class="font-bold text-xl"> Features </h2>
+<ul class="list-disc list-inside">
+<li class="mt-2"> ${data.features['1'].feature_name}  </li>
+<li class="mt-2"> ${data.features['2'].feature_name}  </li>
+<li class="mt-2"> ${data.features['3'].feature_name}  </li>
+</ul>
+</div>
+
+<div>
+<h2 class="font-bold text-xl"> Integration </h2>
+<ul class="list-disc list-inside">
+<li class="mt-2"> ${data.integrations[0]}  </li>
+<li class="mt-2"> ${data.integrations[1]}  </li>
+<li class="mt-2"> ${data.integrations[2]}  </li>
+</ul>
+</div>
+
+</div>
+
+</div>
+
+`;
+
+modalContainer.appendChild(divLeft);
+
+divRight.innerHTML = `
+<figure class="w-4/5 mt-2 mx-auto">
+<img src="${data.image_link[1]}">
+<div class="badge badge-secondary absolute top-3 custom-right bg-red-700 text-white">${data.accuracy.score*100} % accuracy</div>
+</figure> 
+<div class="card-body">
+<h2 class="card-title font-bold mt-4">${data.input_output_examples[0].input
+}
+</h2> 
+<p class="mt-4 text-lg">${data.input_output_examples[0].output}</p> 
+
+</div>
+
+`;
+
+modalContainer.appendChild(divRight);
 
 }
 
